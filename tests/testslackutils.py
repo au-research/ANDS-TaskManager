67686787b23d96d36319b3e5720bbea9df0e05d9
Leo Monus <leo.monus@ardc.edu.au>
2023-08-03 11:22:34 +1000

    RDA-956 Make Unit-Testing Clean Again

--- tests/testslackutils.py
+++ tests/testslackutils.py
@@ -1,29 +1,35 @@
+import random
 import unittest
 import myconfig
 import requests
 from utils.SlackUtils import SlackUtils
 
 """
-https://rapidapi.com/KegenGuyll/api/dad-jokes/
-free (50 pd)
+https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes
+free and clean (3000 p/m)
 """
+
+
 def get_joke_message():
-    url = "https://dad-jokes.p.rapidapi.com/random/joke"
+    url = "https://dad-jokes-by-api-ninjas.p.rapidapi.com/v1/dadjokes"
 
     if not(hasattr(myconfig, "X_RapidAPI_Key")) or myconfig.X_RapidAPI_Key.strip() == '':
         return "No X_RapidAPI_Key, No Jokes for you!"
 
     headers = {
         "X-RapidAPI-Key": myconfig.X_RapidAPI_Key,
-        "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
+        "X-RapidAPI-Host": "dad-jokes-by-api-ninjas.p.rapidapi.com"
     }
+
+    faces = [":troll:", ":ninja:", ":thinking_face:", ":zany_face:", ":woozy_face:", ":face_with_peeking_eye:", ":dotted_line_face:",
+             ":face_with_rolling_eyes:", ":cat:", ":clown_face:", ":shrug:",
+             ":face_palm:"]
     try:
         response = requests.get(url, headers=headers)
         joke = response.json()
-        setup = joke['body'][0]["setup"]
-        punchline = joke['body'][0]["punchline"]
-        return "\n" + setup + '\n:thinking_face:\n' + punchline + ":zany_face:"
-    except Exception:
+        return random.choice(faces) + "\n" + joke[0]['joke'] + "\n" + random.choice(faces)
+    except Exception as e:
+        print(e)
         return "No Joke for you!"
 
 
@@ -44,5 +50,3 @@ class TestSlackUtils(unittest.TestCase):
         message = get_joke_message()
         response = slack_util.post_message("Unit-Test Resume msg:" + message, task_info, "RESUME")
         self.assertTrue(response == 200)
-
-
